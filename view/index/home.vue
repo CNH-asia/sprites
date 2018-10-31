bfca100e1f86888452cee965c6ede25cfd22975c
chenljy19172 <chenljy19172@hs.handsome.com.cn>
2018-10-31 20:51:31 +0800

    sprites

---
+++ view/index/home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="title">
+            <code class="more"></code>
+            <span>首 页</span>
+            <code class="scan"></code>
+        </div>
+        <div class="main">
+            <h1>欢迎来到Light！</h1>
+            <span class="item scan"></span>
+            <span class="item home"></span>
+            <span class="item about"></span>
+
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {}
+        }
+    }
+</script>
+<style lang="less">
+
+</style>
